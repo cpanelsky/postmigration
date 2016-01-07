2cece1bbb451f8926709f744eb9f5f577518f6f2
cP_Sky <10bserver8@gmail.com>
2016-01-06 18:56:56 -0700

    Update postmigration.pl

--- postmigration.pl
+++ postmigration.pl
@@ -27,43 +27,53 @@ my $transferror = 0;
 my $humanrun    = "1";
 our %domains;
 our $file_name = "/etc/userdatadomains";
-our @links     = read_file( $file_name );
+our @links     = read_file($file_name);
 our $link_ref  = \@links;
 our $VERSION   = 0.2;
 our $dns_toggle;
 
-GetOptions( 'mail'  => \$mail,
-            'ipdns' => \$ipdns,
-            'all'   => \$all,
-            'hosts' => \$hosts,
-            'json'  => \$jsons,
-            'local' => \$localcheck,
-            'tterr' => \$transferror,
-            'help!' => \$help );
+GetOptions(
+    'mail'  => \$mail,
+    'ipdns' => \$ipdns,
+    'all'   => \$all,
+    'hosts' => \$hosts,
+    'json'  => \$jsons,
+    'local' => \$localcheck,
+    'tterr' => \$transferror,
+    'help!' => \$help
+);
 
-if ( $localcheck ) {
+if ($localcheck) {
     $dns_toggle = "localhost";
-} else {
+}
+else {
     $dns_toggle = "8.8.8.8";
 }
-if ( $help ) {
+if ($help) {
     &helpsub();
-} elsif ( $jsons ) {
+}
+elsif ($jsons) {
     $humanrun = "0";
     &get_webrequest;
-} elsif ( $transferror ) {
+}
+elsif ($transferror) {
     &transfer_errors();
-} elsif ( $mail ) {
+}
+elsif ($mail) {
     &get_mail_accounts();
-} elsif ( $ipdns ) {
+}
+elsif ($ipdns) {
     &get_webrequest;
-} elsif ( $all ) {
+}
+elsif ($all) {
     &get_webrequest;
     &gen_hosts_file();
     &get_mail_accounts();
-} elsif ( $hosts ) {
+}
+elsif ($hosts) {
     &gen_hosts_file();
-} else {
+}
+else {
     &helpsub();
 }
 
@@ -87,22 +97,23 @@ sub http_web_request {
     require LWP::UserAgent;
     $SIG{'INT'} = sub { print "\nCaught CTRL+C!.."; print RESET " Ending..\n"; kill HUP => -$$; };
     my $url = $_[0];
-    if ( $url ) {
+    if ($url) {
         my $ua = LWP::UserAgent->new( agent => 'Mozilla/5.0', timeout => '1' );
         my $req   = HTTP::Request->new( GET => "http://$url" );
         my $reqIP = "NULL";
         my $code  = "NULL";
-        my $res   = $ua->request( $req );
+        my $res   = $ua->request($req);
         my $body  = $res->decoded_content;
         $code = $res->code();
         my $headervar = $res->headers()->as_string;
-        print $res->header( "content-type\r\n\r\n" );
+        print $res->header("content-type\r\n\r\n");
 
         if ( $headervar =~ /Client-Peer:[\s](.*):([0-9].*)/ ) {
             $reqIP = "$1:$2";
-        } else {
+        }
+        else {
             $reqIP = "NULL";
-            chomp( $reqIP );
+            chomp($reqIP);
         }
         if ( not defined $code ) {
             $code = "NULL";
@@ -117,16 +128,16 @@ sub http_web_request {
 
 sub dns_web_request {
     $SIG{'INT'} = sub { print "\nCaught CTRL+C!.."; print RESET " Ending..\n"; kill HUP => -$$; };
-    my $url        = $_[0];
-    if ( $url ) {
+    my $url = $_[0];
+    if ($url) {
         my $domain        = $url;
         my $google_dns    = "NULL";
         my $localhost_dns = "NULL";
         my $cmd           = "dig";
         my @local_args    = ( "\@localhost", "$domain", "A", "+short", "+tries=1" );
         my @google_args   = ( "\@$dns_toggle", "$domain", "A", "+short", "+tries=1" );
-        my @google_dnsa    = capture( $cmd, @google_args );
-           $google_dns = $google_dnsa[0];
+        my @google_dnsa   = capture( $cmd, @google_args );
+        $google_dns = $google_dnsa[0];
         my @localhost_dnsa = capture( $cmd, @local_args );
         $localhost_dns = $localhost_dnsa[0];
 
@@ -149,11 +160,13 @@ sub dns_web_request {
                 }
                 if ( $key eq "Domain" ) {
                     printf( " %s %-30s", $key, $value );
-                } else {
+                }
+                else {
                     print "$key:$value ";
                 }
             }
-        } elsif ( $humanrun eq "0" ) {
+        }
+        elsif ( $humanrun eq "0" ) {
             my $jsondata = encode_json \%domains;
             print "$jsondata\n";
         }
@@ -163,15 +176,15 @@ sub dns_web_request {
 
 sub get_webrequest {
     $SIG{'INT'} = sub { print "\nCaught CTRL+C!.."; print RESET " Ending..\n"; kill HUP => -$$; };
-    foreach my $uDomain ( @links ) {
+    foreach my $uDomain (@links) {
         if ( $uDomain =~ /(.*):[\s]/ ) {
             our $resource = $1;
-            &http_web_request( "$resource" );
-            sleep( .5 );
-            &dns_web_request( "$resource" );
+            &http_web_request("$resource");
+            sleep(.5);
+            &dns_web_request("$resource");
         }
     }
-	print "\n"; 
+    print "\n";
 }
 
 sub get_mail_accounts {
@@ -179,13 +192,14 @@ sub get_mail_accounts {
     use File::Slurp qw(read_file);
 
     #read in users from passwd
-    my @passwd = read_file( "/etc/passwd" );
+    my @passwd = read_file("/etc/passwd");
     my $dir    = '/var/cpanel/users';
     my %user_list;
-    opendir( DIR, $dir ) or die $!;
-    while ( my $file = readdir( DIR ) ) {
+    my $DIR;
+    opendir( $DIR, $dir ) or die $!;
+    while ( my $file = readdir($DIR) ) {
         next if ( $file =~ m/^\./ );
-        foreach my $line ( @passwd ) {
+        foreach my $line (@passwd) {
 
             #if we look like a system and cpanel user?
             if ( $line =~ /^$file:[^:]*:[^:]*:[^:]*:[^:]*:([a-z0-9_\/]+):.*/ ) {
@@ -193,32 +207,33 @@ sub get_mail_accounts {
             }
         }
     }
-    closedir( DIR );
+    closedir($DIR);
 
     #for the users found, if we aren't root look for an etc dir
     foreach my $user ( keys %user_list ) {
         if ( $user ne "root" ) {
-            opendir( ETC, "$user_list{$user}/etc" ) || warn $! . "$user_list{$user}/etc";
+            my $ETC;
+            opendir( $ETC, "$user_list{$user}/etc" ) || warn $! . "$user_list{$user}/etc";
             my $path = $user_list{$user};
 
             #for the domains found in the users etc dir
-            while ( my $udomain = readdir( ETC ) ) {
-                next if $udomain =~ /^\./;  # skip . and .. dirs
-                                            #see if we are a valid etc domain and if so, look for mail users and print
+            while ( my $udomain = readdir($ETC) ) {
+                next if $udomain =~ /^\./;    # skip . and .. dirs
+                                              #see if we are a valid etc domain and if so, look for mail users and print
                 if ( -d "$path/etc/$udomain/" ) {
-                    open( PASSWD, "$path/etc/$udomain/passwd" ) || die $! . "/home/$user/etc/$udomain/passwd";
-                    while ( my $PWLINE = <PASSWD> ) {
+                    my $PASSWD;
+                    open( $PASSWD, "$path/etc/$udomain/passwd" ) || die $! . "/home/$user/etc/$udomain/passwd";
+                    while ( my $PWLINE = <$PASSWD> ) {
                         $PWLINE =~ s/:.*//;    # only show line data before first colon (username only)
                         chomp( $user, $udomain, $PWLINE );
                         my $PWLINED = "$PWLINE\@$udomain";
-                        chomp( $PWLINED );
+                        chomp($PWLINED);
                         printf( "User=%-10s Domain=%-35s Email=%s\n", $user, $udomain, $PWLINED );
                     }
-                    close( PASSWD );
+                    close($PASSWD);
                 }
             }
         }
-        close( ETC );
     }
     print "\n";
 }
@@ -229,16 +244,17 @@ sub transfer_errors {
     my $transfer_logdir = "/var/cpanel/transfer_sessions";
     my @files;
 
-    dir( "$transfer_logdir" )->recurse(
+    dir("$transfer_logdir")->recurse(
         callback => sub {
             my $file = shift;
             if ( $file =~ /master.log/ ) {
                 push @files, $file->absolute->stringify;
             }
-        } );
+        }
+    );
 
-    foreach my $filename ( @files ) {
-        &find_pkgacct_errors( "$filename" );
+    foreach my $filename (@files) {
+        &find_pkgacct_errors("$filename");
     }
 }
 
@@ -246,14 +262,14 @@ sub find_pkgacct_errors {
     my %seen;
     my @error_list;
     my $log_file      = $_[0];
-    my $last_mod_time = ( stat( $log_file ) )[9];
-    my $humantime     = localtime( $last_mod_time );
+    my $last_mod_time = ( stat($log_file) )[9];
+    my $humantime     = localtime($last_mod_time);
+    my $INPUTFILE;
     print "\n$log_file \n\t ->  dated  -> $humantime -> errors: \n\n";
-    open( INPUTFILE, "<$log_file" ) or die "$!";
+    open( $INPUTFILE, "<$log_file" ) or die "$!";
 
-    while ( <INPUTFILE> ) {
-        if ( $_ =~ m/ was not successful, or the requested account, (.*) was not found on the server: (.*)”\.","/ )
-        {
+    while (<$INPUTFILE>) {
+        if ( $_ =~ m/ was not successful, or the requested account, (.*) was not found on the server: (.*)”\.","/ ) {
             my $account = $1;
             my $server  = $2;
             $account =~ s/\W//g;
@@ -262,7 +278,7 @@ sub find_pkgacct_errors {
         }
     }
     my @unique_error = grep { !$seen{$_}++ } @error_list,;
-    foreach ( @unique_error ) {
+    foreach (@unique_error) {
         if ( $_ =~ /(.*)[\s+](.*)/ ) {
             printf( "Account: %-16s encountered pkgacct/cpmove errors from $2\n", $1 );
         }
@@ -278,9 +294,10 @@ sub gen_hosts_file {
             my ( $new_domain, $user_name, $user_group, $domain_status, $primary_domain, $home_dir, $IP_port ) =
                 split /==/,
                 $host_domain, 9;
-            my ( $IP ) = split /:/, $IP_port, 2;
+            my ($IP) = split /:/, $IP_port, 2;
             print "$IP\t\t$new_domain\twww.$new_domain\n";
-        } else {
+        }
+        else {
             next;
         }
     }
